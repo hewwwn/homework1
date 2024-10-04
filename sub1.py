955bea10fd300b64886febecd70b0b571e381def
hewwwn <jhw001013@kyonggi.ac.kr>
2024-10-05 07:10:13 +0900

    feature-addition Initial Commit

--- sub1.py
+++ sub1.py
@@ -1,8 +1,8 @@
 def calculator():
     while True:
-        operation = input("연산 선택 (+ ,  -  ): ")
-        if operation not in ['+', '-']:
-            print("error 잘못된 연산임. + , - 선택 요망.")
+        operation = input("연산 선택 (+ ,  -  ,  *  ,  /): ")
+        if operation not in ['+', '-', '*', '/']:
+            print("error 잘못된 연산임. + , - , *, / 선택 요망.")
             continue
         try:
             num1 = int(input("첫 번째 숫자를 입력: "))
@@ -13,8 +13,17 @@ def calculator():
 
         if operation == '+':
             result = num1 + num2
-        else:
+        elif operation == '-':
             result = num1 - num2
+        elif operation == '*':
+            result = num1*num2
+        elif operation == '/':
+            if num2 != 0:
+                result = num1/num2
+            else:
+                print("error 0으로 나눌 수 없음.")
+                continue
+                                
 
         print(f"결과: {result}")
         cont = input("다시 계산하기 (y/n): ")
